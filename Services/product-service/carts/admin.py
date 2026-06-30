1dc60c25f9a8e5f9dbdbc4db3bbbc2af1091bc6f
mhmdheydarii <heidarimohammadhosin@gmail.com>
2026-06-30 11:18:16 -0700

    create models for products

--- Services/product-service/carts/admin.py
+++ Services/product-service/carts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from.models import ProductModel
 
 # Register your models here.
+@admin.register(ProductModel)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["name", "status", "created_date", "updated_date"]
+    search_fields = ["name"]
+
--- Services/product-service/carts/models.py
+++ Services/product-service/carts/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+
+class ProductModel(models.Model):
+
+    class ProductStatusType(models.TextChoices):
+        publish = "show", "نمایش"
+        hidden = "hidden", "در انتظار نمایش"
+        rejected = "rejected", "رد شده"
+
+    slug = models.SlugField(unique=True)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    image = models.ImageField(upload_to="media/product")
+
+    status = models.CharField(choices=ProductStatusType.choices, default=ProductStatusType.hidden.value)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ["-created_date"]
+
